12a9fd921760ff671a1834190dd288dce4d38d0f
Victor <wachiravictor12@gmail.com>
2022-05-13 10:44:01 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState , useEffect }from 'react';
+import './App.css'
+import Head from './Head'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const LOCAL_STORAGE = 'todoApp.todo'
+function Todos({ todos , index ,completeTodo , deleteTodos}) {
+  return(
+    <div>
+    <div className="check" >
+    <div onClick={()=>completeTodo(index)} className="box">
+    <Checkbox />
     </div>
-  );
+    <div style={{textDecoration:todos.isCompleted?"line-through":""}} className = "style">
+    {todos.text} 
+    </div>
+    <div >
+   <button onClick={() => deleteTodos(index) }className ="btn" >Delete</button> 
+   </div>
+   </div>
+   <hr/>
+   </div>
+  )
+}
+function TodoForm({addTodos}) {
+  const [value , setValue] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return ;
+    addTodos(value);
+   setValue('');
 }
 
-export default App;
+  return(
+    <form onSubmit ={handleSubmit}>
+      <input type ='text' 
+      className ="input"
+       value ={value}
+       onChange={e => setValue(e.target.value)}
+       placeholder ="Add Todo"/>
+    </form>
+  )
+}
+function Checkbox() {
+  const [chkValue, setChkValue] = useState();
+return (
+   <div className="txt">
+      <input type="checkbox"
+      checked={chkValue} 
+      onClick={()=>setChkValue(!chkValue)}
+      />
+   </div>
+  );
+}
+function App() {
+   const [todo,setTodo]= useState([]);
+   useEffect(() => {
+       const storedTodo = JSON.parse(localStorage.getItem(LOCAL_STORAGE))
+       if(storedTodo) setTodo(storedTodo)
+    }, [])
+  
+
+   useEffect(() => {
+       localStorage.setItem(LOCAL_STORAGE,JSON.stringify(todo))
+   },[todo])
+
+   const addTodos = text => {
+     const newTodo = [...todo,{ text }]
+     setTodo(newTodo);
+  }
+  const completeTodo = index => {
+    const newTodo = [...todo];
+    newTodo[index].isCompleted = true;
+    setTodo(newTodo);
+  }
+   const deleteTodos = index => {
+    const newTodo = [...todo];
+    newTodo.splice(index, 1);
+    setTodo(newTodo);
+  }
+    return (
+    <div>
+       <Head/>
+       
+      <div className= "todo">
+      <TodoForm addTodos = {addTodos} />
+         {todo.map((todos,index)  => (
+           <Todos key ={index}
+            index = {index}
+             todos={todos} 
+             completeTodo={completeTodo}
+              deleteTodos = {deleteTodos}/>
+              ))}
+      </div>
+    </div>
+
+    )
+    
+}
+
+
+export default App
---
+++ src/Head.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css"
+
+export const Head = () => {
+  return (
+    <div>
+        <h2 className='head'>Todo List</h2>
+    </div>
+  )
+}
+export default Head
